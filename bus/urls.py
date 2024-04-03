206d9892a79e9aa8760d87ad22ad25b5a1900c68
RajputanaM <92735849+RajputanaM@users.noreply.github.com>
2024-04-03 22:57:13 +0530

    Update urls.py

--- bus/urls.py
+++ bus/urls.py
@@ -3,13 +3,15 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
+
     path('buses/', views.BusListCreateView.as_view(), name='bus-list-create'),
     path('buses/<int:pk>/', views.BusDetailView.as_view(), name='bus-detail'),
-
+      
     # URLs for listing and creating tickets
     path('tickets/', views.TicketListCreateView.as_view(), name='ticket-list-create'),
     path('ticket/<int:pk>/', views.TicketDetailView.as_view(), name='ticket-detail'),
     path('tickets/<int:user_id>/', views.TicketListByUserView.as_view(), name='ticket-list-by-user'),
+    path('check_ticket/', views.check_ticket, name='check_ticket_exist'),
 
     # Custom URL for searching buses
     path('buses/search/', views.BusSearchView.as_view(), name='bus-search'),
@@ -24,30 +26,37 @@ urlpatterns = [
     path('verifyOtp/',views.verify_otp_view),
     path('registration/',views.registration_view, name = 'registration'),
     
+
+
     path('userByEmail/<str:email>', views.GetUserByEmail.as_view()),
     path('updateuser/<str:email>', views.UserProfileUpdateView.as_view()),
     path('otpPassword/',views.verify_otp_view2),
 
+    path('bus/<int:bus_id>/', views.tickets_by_bus_view, name='tickets_by_bus'),
+    path('bus/<int:bus_id>/<str:date>/', views.tickets_by_date, name='tickets_by_date'),
+
+
+    path('conductors/',views.ConductorList.as_view()),
+    path('conductor/login/', views.conductor_login, name='conductor-login'),
+    path('getbusbyemail/', views.get_bus_id_by_email, name='get_bus_id_by_email'),
 
 
 
 
+    # #chat room
 
-
-
-    #chat room
-
-
-    path('rooms/', views.ChatRoomViewSet.as_view({'get': 'list', 'post': 'create'}), name='chat-room-list'),
-    path('chat-rooms/<int:pk>/', views.ChatRoomViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='chat-room-detail'),
-    path('chat-rooms/<int:pk>/add-member/', views.ChatRoomViewSet.as_view({'post': 'add_member'}), name='chat-room-add-member'),
+    # path('rooms/', views.ChatRoomViewSet.as_view({'get': 'list', 'post': 'create'}), name='chat-room-list'),
+    # path('chat-rooms/<int:pk>/', views.ChatRoomViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='chat-room-detail'),
+    # path('chat-rooms/<int:pk>/add-member/', views.ChatRoomViewSet.as_view({'post': 'add_member'}), name='chat-room-add-member'),
 
     # Member URLs
-    path('members/', views.MemberViewSet.as_view({'get': 'list', 'post': 'create'}), name='member-list'),
-    path('members/<int:pk>/', views.MemberViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='member-detail'),
+    # path('members/', views.MemberViewSet.as_view({'get': 'list', 'post': 'create'}), name='member-list'),
+    # path('members/<int:pk>/', views.MemberViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='member-detail'),
 
     # Message URLs
-    path('messages/', views.MessageViewSet.as_view({'get': 'list', 'post': 'create'}), name='message-list'),
-    path('messages/<int:pk>/add-message/', views.MessageViewSet.as_view({'post': 'add_message'}), name='message-add-message'),
-    path('messages/<int:room_id>/', views.MessageViewSet1.as_view({'get': 'messages_by_room'}), name='messages-by-room'),
+    # path('messages/', views.MessageViewSet.as_view({'get': 'list', 'post': 'create'}), name='message-list'),
+    # path('messages/<int:pk>/add-message/', views.MessageViewSet.as_view({'post': 'add_message'}), name='message-add-message'),
+    # path('messages/<int:room_id>/', views.MessageViewSet1.as_view({'get': 'messages_by_room'}), name='messages-by-room'),
+
 ]
+
